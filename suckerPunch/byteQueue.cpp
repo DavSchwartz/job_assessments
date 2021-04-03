0299ff07b2ab6b960acc10f160fd5518a8f335c0
David Schwartz <dmschwartz01@gmail.com>
2021-04-03 13:41:33 -0500

    finish implementation

--- suckerPunch/byteQueue.cpp
+++ suckerPunch/byteQueue.cpp
@@ -1,4 +1,5 @@
 #include <math.h>
+#include <stdio.h>
 
 unsigned char * create_queue();
 void destroy_queue(unsigned char * q);
@@ -6,36 +7,36 @@ void enqueue_byte(unsigned char * q, unsigned char b);
 unsigned char dequeue_byte(unsigned char * q);
 void on_out_of_memory();
 void on_illegal_operation();
-void setQueueIndicesUsageBit(int queue_indices_index, bool set);
-void setUsageBit(int dynamic_mem_index, bool set);
-bool isUsageBitSet(int dynamic_mem_index);
-unsigned short getQueueStartIndex(unsigned char * q);
-unsigned short getQueueEndIndex(unsigned char * q);
-void setQueueStartIndex(unsigned char * q, unsigned short val);
-void setQueueEndIndex(unsigned char * q, unsigned short val);
-unsigned char * getAvailQueueIndex();
-void freeQueueIndex(int queue_index);
-unsigned short globalChunkIndex(unsigned char chunk_index);
+unsigned short queueStartIndex(unsigned char queue_num);
+unsigned short queueEndIndex(unsigned char queue_num);
+void setQueueStartIndex(unsigned char queue_num, unsigned short val);
+void setQueueEndIndex(unsigned char queue_num, unsigned short val);
+unsigned char allocQueue();
+void freeQueue(unsigned char queue_num);
+unsigned char allocChunk();
+void freeChunk(unsigned char chunk_num);
+unsigned char chunkNum(unsigned short index);
+unsigned short nextChunkNumIndex(unsigned char chunk_num);
+unsigned short chunkIndex(unsigned char chunk_num);
+unsigned char queueNum(unsigned short index);
+unsigned short queueIndex(unsigned char queue_num);
 void init();
 int main(int argc, char **argv);
 
+#define ALL_CHUNKS_INDEX 0
+#define ALL_CHUNKS_LEN 1848
+#define ALL_QUEUES_INDEX 1848
+#define ALL_QUEUES_LEN 192
+#define AVAIL_QUEUE_NUM_INDEX 2040
+#define AVAIL_QUEUE_NUM_LEN 1
+#define AVAIL_CHUNK_NUM_INDEX 2041
+#define AVAIL_CHUNK_NUM_LEN 1
+#define INIT_INDEX 2042
 #define CHUNK_LEN 8
-#define TOTAL_LEN 2048
-#define DYNAMIC_MEMORY_INDEX 0
-#define DYNAMIC_MEMORY_LEN 1815
-#define DATA_INDEX 0
-#define DATA_LEN 1624
-#define QUEUE_INDICES_INDEX 1624
-#define QUEUE_INDICES_LEN 192
-#define QUEUE_INDEX_LEN 3
-#define USAGE_BITS_INDEX 1816
-#define USAGE_BITS_LEN 227
-#define AVAIL_QUEUE_INDICES_INDEX 2043
-#define AVAIL_QUEUE_INDICES_LEN 2
-#define AVAIL_CHUNK_INDICES_INDEX 2035
-#define AVAIL_CHUNK_INDICES_LEN 2
-#define INIT_INDEX 2048
-#define INIT_LEN 1
+#define QUEUE_LEN 3
+#define NUM_CHUNKS 231
+#define NUM_QUEUES 64
+#define DATA_LEN 2048
 #define BITS_IN_BYTE 8
 #define INVALID 0xff
 
@@ -43,26 +44,22 @@ int main(int argc, char **argv);
 
 {} = number of occurances
 
-data: [dynamic_memory{1}: 1815 byte, avail_queue_indices{1}: 2 bytes, avail_chunk_indices{1}: 2 bytes, init{1}: 1 byte]
+data: [chunk{231}: 1848 bytes, queue{64}: 192 bytes, avail_queue_number: 1 bytes, avail_chunk_number: 1 bytes, init: 1 byte, unused: 5 bytes]
+ 
+When not in use, next_chunk_number is the number of the next unused chunk
+Chunk number counted from zero
+chunk = entry{7}: 7 bytes, next_chunk_number{1}: 1 byte
+entry = uint8 data entry: 1 byte
+next_chunk_number = uint8 chunk number containing subsequent data: 1 byte
 
-dynamic_memory = data_chunk{203}: 1624 bytes, queue_indices{64}: 192 bytes
-
-data_chunk length must be divisible by 8
-data_chunk = data_entry{7}: 7 bytes, chunk_index{1}: 1 byte
-data_entry = uint8 data entry: 1 byte
-chunk_pointer = uint8 index of next data_chunk: 1 byte
-
-queue_indices length must be divisible by 3
-queue_indices = queue_start_index{1}: 1.5 bytes, queue_end_index{1}: 1.5 bytes
+When not in use, first byte of queue is the number of the next unused queue
+Queue number counted from zero
+queue = queue_start_index{1}: 1.5 bytes, queue_end_index{1}: 1.5 bytes
 queue_start_index = uint12 index of start of queue: 1.5 bytes
 queue_end_index = uint12 index of end of queue: 1.5 bytes
 
-usage_bits = boolean indicator of memory usage: 1 bit; 0: available; 1: in use
-
-avail_queue_indices = prev_avail_index{1}: 1 byte, next_avail_index{1}: 1 byte
-avail_chunk_indices = prev_avail_index{1}: 1 byte, next_avail_index{1}: 1 byte
-prev_avail_index = uint8 index of available memory, pointing to next_avail_index: 1 byte
-next_avail_index = uint8 index of available memory: 1 byte
+avail_queue_index = uint16 index of available queue
+avail_chunk_index = uint16 index of avail chunk
 
 init: uint8 initialization indicator: 0: uninitialized; 1: initialized
 
@@ -72,9 +69,10 @@ static unsigned char data[DATA_LEN] = {0};
 
 unsigned char * create_queue()
 {
-	init();
+	if (data[INIT_INDEX] == 0) { init(); }
 
-	return getAvailQueueIndex();
+	return data + queueIndex(allocQueue());
+
 }
 
 void destroy_queue(unsigned char * q)
@@ -84,6 +82,22 @@ void destroy_queue(unsigned char * q)
 		on_illegal_operation();
 	}
 
+	unsigned char queue_number = queueNum(q - data);
+	unsigned short start_index = queueStartIndex(queue_number);
+	unsigned short end_index = queueEndIndex(queue_number);
+	unsigned char start_chunk_num = chunkNum(start_index);
+	unsigned char end_chunk_num = chunkNum(end_index);
+	unsigned char current_chunk_num = start_chunk_num;
+
+	while (current_chunk_num != end_chunk_num)
+	{
+		unsigned char next_chunk_num = data[nextChunkNumIndex(current_chunk_num)];
+		freeChunk(current_chunk_num);
+		current_chunk_num = next_chunk_num;
+	}
+	freeChunk(end_chunk_num);
+
+	freeQueue(queue_number);
 }
 
 void enqueue_byte(unsigned char * q, unsigned char b)
@@ -93,36 +107,74 @@ void enqueue_byte(unsigned char * q, unsigned char b)
 		on_illegal_operation();
 	}
 
-	unsigned short start_index = getQueueStartIndex(q);
-	unsigned short end_index = getQueueStartIndex(q);
+	unsigned char queue_number = queueNum(q - data);
+	unsigned short start_index = queueStartIndex(queue_number);
+	unsigned short end_index = queueEndIndex(queue_number);
+	unsigned char end_chunk_num = chunkNum(end_index);
+
+	if ((start_index == INVALID) || (end_index == INVALID))
+	{
+		unsigned char new_chunk_number = allocChunk();
+		data[chunkIndex(new_chunk_number)] = b;
+		start_index = chunkIndex(new_chunk_number);
+		end_index = start_index + 1;
+	}
+	// next index represents next_chunk_number
+	else if (end_index == nextChunkNumIndex(end_chunk_num))
+	{
+		unsigned char new_chunk_number = allocChunk();
+		data[end_index] = new_chunk_number;
+		data[chunkIndex(new_chunk_number)] = b;
+		end_index = chunkIndex(new_chunk_number);
+	}
+	else
+	{
+		data[end_index] = b;
+		end_index++;
+	}
+
+	setQueueStartIndex(queue_number, start_index);
+	setQueueEndIndex(queue_number, end_index);
 }
 
 unsigned char dequeue_byte(unsigned char * q)
 {
-	if ((data[INIT_INDEX] == 0) ||
-		!isUsageBitSet(q - data - DYNAMIC_MEMORY_INDEX) ||
-		!isUsageBitSet(q - data - DYNAMIC_MEMORY_INDEX)
-	)
+	if (data[INIT_INDEX] == 0)
 	{
 		on_illegal_operation();
 	}
 
-	unsigned short start_index = getQueueStartIndex(q);
-	unsigned short end_index = getQueueStartIndex(q);
+	unsigned char queue_number = queueNum(q - data);
 
-	setUsageBit((start_index - DYNAMIC_MEMORY_INDEX), false);
+	unsigned short start_index = queueStartIndex(queue_number);
+	unsigned short end_index = queueEndIndex(queue_number);
+	unsigned char out = data[start_index];
+	unsigned char start_chunk_num = chunkNum(start_index);
 
-	if ((start_index % CHUNK_LEN) = (CHUNK_LEN - 1))
+	if ((start_index == INVALID) || (end_index == INVALID))
 	{
-		return data[data[start_index - DYNAMIC_MEMORY_INDEX] * CHUNK_LEN];
+		on_illegal_operation();
+	}
+	else if ((start_index + 1) == end_index)
+	{
+		start_index = INVALID;
+		end_index = INVALID;
+	}
+	// next index represents next_chunk_number
+	else if ((start_index + 1) == nextChunkNumIndex(start_chunk_num))
+	{
+		start_index = chunkIndex(data[start_index + 1]);
+		freeChunk(start_chunk_num);
 	}
 	else
 	{
-
+		start_index++;
 	}
 
+	setQueueStartIndex(queue_number, start_index);
+	setQueueEndIndex(queue_number, end_index);
 
-	return data[start_index - DYNAMIC_MEMORY_INDEX];
+	return out;
 }
 
 void on_out_of_memory()
@@ -135,206 +187,238 @@ void on_illegal_operation()
 	for (;;) {}
 }
 
-void setQueueIndicesUsageBit(int queue_indices_index, bool set)
+unsigned short queueStartIndex(unsigned char queue_num)
 {
-	int dynamic_mem_index = QUEUE_INDICES_INDEX - DYNAMIC_MEMORY_INDEX + queue_indices_index;
-	setUsageBit(dynamic_mem_index, set);
-	setUsageBit(dynamic_mem_index + 1, set);
-	setUsageBit(dynamic_mem_index + 2, set);
+	unsigned short queue_index = queueIndex(queue_num);
+
+	unsigned char val_4_least_sig = (data[queue_index + 1] & 0xf0) >> 4;
+	unsigned char val_8_most_sig = data[queue_index];
+	return (val_8_most_sig << 4) | val_4_least_sig;
 }
 
-void setUsageBit(int dynamic_mem_index, bool set)
+unsigned short queueEndIndex(unsigned char queue_num)
 {
-	int usage_bit_index = (int)floor(dynamic_mem_index / BITS_IN_BYTE);
-	int usage_bit_number = (int)(dynamic_mem_index % BITS_IN_BYTE);
-	unsigned char usage_bits = data[USAGE_BITS_INDEX + usage_bit_index];
-	if (set)
-	{
-		unsigned char new_usage_bits = 0x80 >> usage_bit_number;
-		data[USAGE_BITS_INDEX + usage_bit_index] = new_usage_bits | new_usage_bits;
-	}
-	else
-	{
-		unsigned char new_usage_bits = (unsigned char)(0xff7f >> usage_bit_number);
-		data[USAGE_BITS_INDEX + usage_bit_index] = new_usage_bits & new_usage_bits;
-	}
+	unsigned short queue_index = queueIndex(queue_num);
+
+	unsigned char val_4_most_sig = (data[queue_index + 1] & 0x0f);
+	unsigned char val_8_least_sig = data[queue_index + 2];
+	return (val_4_most_sig << 8) | val_8_least_sig;
 }
 
-bool isUsageBitSet(int dynamic_mem_index)
+void setQueueStartIndex(unsigned char queue_num, unsigned short val)
 {
-	int usage_bit_index = (int)floor(dynamic_mem_index / BITS_IN_BYTE);
-	int usage_bit_number = (int)(dynamic_mem_index % BITS_IN_BYTE);
-	unsigned char usage_bits = data[USAGE_BITS_INDEX + usage_bit_index];
-	unsigned char new_usage_bits = usage_bits >> (BITS_IN_BYTE - 1 - usage_bit_number);
-	new_usage_bits = new_usage_bits & 0x01;
-	return new_usage_bits;
-}
+	unsigned short queue_index = queueIndex(queue_num);
 
-unsigned short getQueueStartIndex(unsigned char * q)
-{
-	return (*q << 4) | ((*(q + 1) & 0xf0) >> 4);
-}
-
-unsigned short getQueueEndIndex(unsigned char * q)
-{
-	return ((*(q + 1) & 0x0f) << 8) | *(q+2);
-}
-
-void setQueueStartIndex(unsigned char * q, unsigned short val)
-{
 	unsigned char val_4_least_sig = val & 0x000f;
 	unsigned char val_8_most_sig = (val & 0x0ff0) >> 4;
-	*q = val_8_most_sig;
-	*(q + 1) = (val_4_least_sig << 4) | (*(q + 1) & 0x0f);
+	data[queue_index] = val_8_most_sig;
+	data[queue_index + 1] = (val_4_least_sig << 4) | (data[queue_index + 1] & 0x0f);
 }
 
-void setQueueEndIndex(unsigned char * q, unsigned short val)
+void setQueueEndIndex(unsigned char queue_num, unsigned short val)
 {
+	unsigned short queue_index = queueIndex(queue_num);
+
 	unsigned char val_4_most_sig = (val & 0x0f00) >> 8;
 	unsigned char val_8_least_sig = (val & 0x0ff);
-	*(q + 1) = (*(q + 1) & 0xf0) | val_4_most_sig;
-	*(q + 2) = val_8_least_sig;
+	data[queue_index + 1] = (data[queue_index + 1] & 0xf0) | val_4_most_sig;
+	data[queue_index + 2] = val_8_least_sig;
 }
 
-unsigned char availQueueIndex()
+unsigned char allocQueue()
 {
-	unsigned char* avail_queue_indices_index = &data[AVAIL_QUEUE_INDICES_INDEX];
-	if (*avail_queue_indices_index == INVALID)
+	unsigned char* avail_queue_num = &data[AVAIL_QUEUE_NUM_INDEX];
+	if (*avail_queue_num == INVALID)
 	{
 		on_out_of_memory();
 	}
 
-	unsigned char* avail_chunk = &data[globalChunkIndex(*avail_chunk_index)];
-	unsigned char* next_avail_chunk = &data[globalChunkIndex(*avail_chunk)];
-	unsigned char out = *avail_chunk;
+	unsigned char* avail_queue_next_num = &data[queueIndex(*avail_queue_num)];
+	unsigned char* next_queue_next_num = &data[queueIndex(*avail_queue_next_num)];
+	unsigned char out = *avail_queue_next_num;
 
-	if (*avail_chunk_index == *avail_chunk)
+	if (*avail_queue_num == *avail_queue_next_num)
 	{
-		*avail_chunk_index = INVALID;
+		*avail_queue_num = INVALID;
 	}
 	else
 	{
-		*avail_chunk = *next_avail_chunk;
+		*avail_queue_next_num = *next_queue_next_num;
+	}
+
+	setQueueStartIndex(out, INVALID);
+	setQueueEndIndex(out, INVALID);
+	
+	return out;
+}
+
+void freeQueue(unsigned char queue_num)
+{
+	unsigned char new_queue_num = queue_num;
+	unsigned char* avail_queue_num = &data[AVAIL_QUEUE_NUM_INDEX];
+	unsigned char* new_queue_next_num = &data[queueIndex(new_queue_num)];
+	if (*avail_queue_num == INVALID)
+	{
+		*avail_queue_num = new_queue_num;
+		*new_queue_next_num = new_queue_num;
+
+	}
+
+	unsigned char* avail_queue_next_num = &data[queueIndex(*avail_queue_num)];
+	if (*avail_queue_num == *avail_queue_next_num)
+	{
+		*avail_queue_next_num = new_queue_num;
+		*new_queue_next_num = *avail_queue_num;
+	}
+	else
+	{
+		*new_queue_next_num = *avail_queue_next_num;
+		*avail_queue_next_num = new_queue_num;
+	}
+}
+
+unsigned char allocChunk()
+{
+	unsigned char* avail_chunk_num = &data[AVAIL_CHUNK_NUM_INDEX];
+	if (*avail_chunk_num == INVALID)
+	{
+		on_out_of_memory();
+	}
+
+	unsigned char* avail_chunk_next_num = &data[nextChunkNumIndex(*avail_chunk_num)];
+	unsigned char* next_chunk_next_num = &data[nextChunkNumIndex(*avail_chunk_next_num)];
+	unsigned char out = *avail_chunk_next_num;
+
+	if (*avail_chunk_num == *avail_chunk_next_num)
+	{
+		*avail_chunk_num = INVALID;
+	}
+	else
+	{
+		*avail_chunk_next_num = *next_chunk_next_num;
 	}
 	
 	return out;
 }
 
-void freeQueueIndex(int new_chunk_index)
+void freeChunk(unsigned char chunk_num)
 {
-	unsigned char* avail_chunk_index = &data[AVAIL_CHUNK_INDEX];
-	unsigned char* new_chunk = &data[globalChunkIndex(new_chunk_index)];
-	if (*avail_chunk_index == INVALID)
+	unsigned char new_chunk_num = chunk_num;
+	unsigned char* avail_chunk_num = &data[AVAIL_CHUNK_NUM_INDEX];
+	unsigned char* new_chunk_next_num = &data[nextChunkNumIndex(new_chunk_num)];
+	if (*avail_chunk_num == INVALID)
 	{
-		*avail_chunk_index = new_chunk_index;
-		*new_chunk = new_chunk_index;
+		*avail_chunk_num = new_chunk_num;
+		*new_chunk_next_num = new_chunk_num;
 
 	}
 
-	unsigned char* avail_chunk = &data[globalChunkIndex(*avail_chunk_index)];
-
-	if (*avail_chunk_index == *avail_chunk)
+	unsigned char* avail_chunk_next_num = &data[nextChunkNumIndex(*avail_chunk_num)];
+	if (*avail_chunk_num == *avail_chunk_next_num)
 	{
-		*avail_chunk = new_chunk_index;
-		*new_chunk = *avail_chunk_index;
+		*avail_chunk_next_num = new_chunk_num;
+		*new_chunk_next_num = *avail_chunk_num;
 	}
 	else
 	{
-		*new_chunk = *avail_chunk;
-		*avail_chunk = new_chunk_index;
+		*new_chunk_next_num = *avail_chunk_next_num;
+		*avail_chunk_next_num = new_chunk_num;
 	}
 }
 
-unsigned char availChunk()
+unsigned char chunkNum(unsigned short index)
 {
-	unsigned char* avail_chunk_index = &data[AVAIL_CHUNK_INDEX];
-	if (*avail_chunk_index == INVALID)
+	if ((index < ALL_CHUNKS_INDEX) || (index >= (ALL_CHUNKS_INDEX + ALL_CHUNKS_LEN)))
 	{
-		on_out_of_memory();
-	}
-
-	unsigned char* avail_chunk = &data[globalChunkIndex(*avail_chunk_index)];
-	unsigned char* next_avail_chunk = &data[globalChunkIndex(*avail_chunk)];
-	unsigned char out = *avail_chunk;
-
-	if (*avail_chunk_index == *avail_chunk)
-	{
-		*avail_chunk_index = INVALID;
-	}
-	else
-	{
-		*avail_chunk = *next_avail_chunk;
+		on_illegal_operation();
 	}
 	
-	return out;
+	return floor((index - ALL_CHUNKS_INDEX) / CHUNK_LEN);
 }
 
-void freeChunk(int new_chunk_index)
+unsigned short nextChunkNumIndex(unsigned char chunk_num)
 {
-	unsigned char* avail_chunk_index = &data[AVAIL_CHUNK_INDEX];
-	unsigned char* new_chunk = &data[globalChunkIndex(new_chunk_index)];
-	if (*avail_chunk_index == INVALID)
-	{
-		*avail_chunk_index = new_chunk_index;
-		*new_chunk = new_chunk_index;
-
-	}
-
-	unsigned char* avail_chunk = &data[globalChunkIndex(*avail_chunk_index)];
-
-	if (*avail_chunk_index == *avail_chunk)
-	{
-		*avail_chunk = new_chunk_index;
-		*new_chunk = *avail_chunk_index;
-	}
-	else
-	{
-		*new_chunk = *avail_chunk;
-		*avail_chunk = new_chunk_index;
-	}
-}
-
-unsigned short globalChunkIndex(unsigned char chunk_index)
-{
-	if (chunk_index == INVALID)
+	if (chunk_num == INVALID)
 	{
 		on_illegal_operation();
 	}
 
-	return DATA_INDEX + (chunk_index * CHUNK_LEN) + (CHUNK_LEN - 1);
+	return ALL_CHUNKS_INDEX + (chunk_num * CHUNK_LEN) + (CHUNK_LEN - 1);
+}
+
+unsigned short chunkIndex(unsigned char chunk_num)
+{
+	if (chunk_num == INVALID)
+	{
+		on_illegal_operation();
+	}
+
+	return ALL_CHUNKS_INDEX + (chunk_num * CHUNK_LEN);
+}
+
+unsigned char queueNum(unsigned short index)
+{
+	if ((index < ALL_QUEUES_INDEX) || (index >= (ALL_QUEUES_INDEX + ALL_QUEUES_LEN)))
+	{
+		on_illegal_operation();
+	}
+	
+	return floor((index - ALL_QUEUES_INDEX) / QUEUE_LEN);
+}
+
+unsigned short queueIndex(unsigned char queue_num)
+{
+	if (queue_num == INVALID)
+	{
+		on_illegal_operation();
+	}
+
+	return ALL_QUEUES_INDEX + (queue_num * QUEUE_LEN);
 }
 
 void init()
 {
-	// init bit not set
-	if (data[INIT_INDEX] == 0)
+	// Make available chunks point to next available chunk
+	for (int i = 0; i < (NUM_CHUNKS - 1); i++)
 	{
-		// Make available chunks point to next available chunk
-		for (int i = (CHUNK_LEN - 1); i < (DATA_LEN - 1); i += CHUNK_LEN)
-		{
-			data[DATA_INDEX + i] = (int)((i + 1) / CHUNK_LEN);
-		}
-		data[DATA_INDEX + DATA_LEN - 1] = 0;
-
-		// Make available queue index locations point to next available available locations
-		for (int i = (QUEUE_INDEX_LEN - 1); i < (QUEUE_INDICES_LEN - QUEUE_INDEX_LEN); i += QUEUE_INDEX_LEN)
-		{
-			data[QUEUE_INDICES_INDEX + i] = i + QUEUE_INDEX_LEN;
-		}
-		data[QUEUE_INDICES_INDEX + QUEUE_INDICES_LEN - QUEUE_INDEX_LEN] = 0;
-
-		// point to last and first available index
-		data[AVAIL_CHUNK_INDICES_INDEX] = (DATA_LEN / CHUNK_LEN) - 1;
-		data[AVAIL_CHUNK_INDICES_INDEX + 1] = 0;
-		data[AVAIL_QUEUE_INDICES_INDEX] = QUEUE_INDICES_LEN - 1;
-		data[AVAIL_QUEUE_INDICES_INDEX + 1] = 0;
-
-		// set init bit
-		data[INIT_INDEX] = 1;
+		data[nextChunkNumIndex(i)] = i + 1;
 	}
+	data[nextChunkNumIndex(NUM_CHUNKS - 1)] = 0;
+
+	// Make available queue index locations point to next available available locations
+	for (int i = 0; i < (NUM_QUEUES - 1); i++)
+	{
+		data[queueIndex(i)] = i + 1;
+	}
+	data[queueIndex(NUM_CHUNKS - 1)] = 0;
+
+	data[AVAIL_QUEUE_NUM_INDEX] = 0;
+	data[AVAIL_CHUNK_NUM_INDEX] = 0;
+
+	data[INIT_INDEX] = 1;
 }
 
 int main(int argc, char **argv)
 {
+	unsigned char* q0 = create_queue();
+	enqueue_byte(q0, 0);
+	enqueue_byte(q0, 1);
+	unsigned char* q1 = create_queue();
+	enqueue_byte(q1, 3);
+	enqueue_byte(q0, 2);
+	enqueue_byte(q1, 4);
+	printf("%d ", dequeue_byte(q0));
+	printf("%d\n", dequeue_byte(q0));
+	enqueue_byte(q0, 5);
+	enqueue_byte(q1, 6);
+	printf("%d ", dequeue_byte(q0));
+	printf("%d\n", dequeue_byte(q0));
+	destroy_queue(q0);
+	printf("%d ", dequeue_byte(q1));
+	printf("%d ", dequeue_byte(q1));
+	printf("%d\n", dequeue_byte(q1));
+	destroy_queue(q1);
+	
+
 	return 0;
 }
